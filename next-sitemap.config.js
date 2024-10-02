bdad5ecba42960bebd30a377d2004d1fa28f58ef
kilian frederix <kilianfrederix@gmail.com>
2024-10-02 17:26:21 +0200

    Revert "added everything needed for testing"
    This reverts commit deaee32fbe9374ac9435f1d23e73ecaa2c19f771.

--- next-sitemap.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  siteUrl: process.env.SITE_URL || 'https://localhost:3000/',
-  generateRobotsTxt: true,
-  robotsTxtOptions: {
-    policies: [{ userAgent: '*', allow: '/' }],
-  },
-};
--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -10,7 +10,6 @@ import { Toaster } from "@/components/ui/toaster";
 import { SiteConfig } from "@/lib/constant";
 import { fonts } from "@/lib/fonts";
 import { cn } from "@/lib/utils";
-import { Analytics } from '@vercel/analytics/react';
 
 export const metadata: Metadata = {
   title: {
@@ -61,10 +60,7 @@ const RootLayout = ({children}: PropsWithChildren) => {
               <Navbar />
             </Suspense>
             <div className="flex grow flex-col">
-              <main className="grow">
-              {children}
-              <Analytics />
-              </main>
+              <main className="grow">{children}</main>
               <SideBar />
               <div className="relative bottom-0 mt-auto">
                 <Footer />
