fa9c1752ebc2dd69d02471bf8cbd52f0fa6ab84e
kilian frederix <kilianfrederix@gmail.com>
2024-10-02 17:50:15 +0200

    error fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,37 @@
-{
-  "extends"; ["next/core-web-vitals", "next/typescript"]
-}
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'next/core-web-vitals',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:tailwindcss/recommended',
+  ],
+  plugins: ['simple-import-sort', '@typescript-eslint'],
+  rules: {
+    'sort-imports': 'off',
+    'tailwindcss/no-custom-classname': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
+    'simple-import-sort/imports': [
+      2,
+      {
+        groups: [
+          ['^.+\\.s?css$'],
+          [
+            `^(${require('module').builtinModules.join('|')})(/|$)`,
+            '^react',
+            '^@?\\w',
+          ],
+          ['^components(/.*|$)'],
+          ['^lib(/.*|$)', '^hooks(/.*|$)'],
+          ['^\\.'],
+        ],
+      },
+    ],
+  },
+  settings: {
+    tailwindcss: {
+      callees: ['cn'],
+      config: 'tailwind.config.js',
+    },
+  },
+};
