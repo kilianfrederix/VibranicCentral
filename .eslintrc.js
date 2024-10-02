dc4bafa79f24208280b200e353cd543fddf8894c
kilian frederix <kilianfrederix@gmail.com>
2024-10-02 17:55:32 +0200

    error fix

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
     'next/core-web-vitals',
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:tailwindcss/recommended',
   ],
   plugins: ['simple-import-sort', '@typescript-eslint'],
   rules: {
